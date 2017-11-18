f87b8f583fbc92e795975139c1c7541854705de6
Thomas Hornschuh <thomas@hornschuh-family.de>
2017-11-18 03:46:35 +0100

    remove unwanted file

--- user/gdb_traps.s
+++
@@ -1,9 +0,0 @@
-# 1 "gdb_traps.S"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 1 "<command-line>" 2
-# 1 "gdb_traps.S"
-.section .text
-.extern handle_exception
-.global __trap
