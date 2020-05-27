38f85505e51435f1828afe8fa966e8d2fff8152e
Thomas Hornschuh <thomas@hornschuh-family.de>
2020-05-27 20:57:20 +0200

    Spiffs makefile commit

---
+++ bonfire-boot/spiffs.mk
@@ -0,0 +1,25 @@
+.phony: test build
+
+spiffs?=spiffs
+
+include spiffs/files.mk
+
+vpath %.c $(CPATH)
+
+OBJFILES = $(CFILES:%.c=${BUILD}/%.o)
+
+include  ../bonfire_defs.Makefile.include
+
+TARGET_CFLAGS += $(INC) -I. $(FLAGS)
+
+TARGET= $(BUILD)/spiffs.a 
+
+$(TARGET) : $(OBJFILES)
+	$(TARGET_AR) r $(TARGET) $(OBJFILES)
+	$(TARGET_PREFIX)-ranlib $(TARGET)
+	$(TARGET_SIZE) $(TARGET) 
+
+build: $(TARGET)
+
+test:
+	echo $(OBJFILES)
